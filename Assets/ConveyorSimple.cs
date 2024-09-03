3c6951f647cfc962e9dece64712b052f36cbee89
41088938 <230292130@stu.vtc.edu.hk>
2024-09-03 17:24:09 +0800

    conveyor belt
    non model

---
+++ Assets/ConveyorSimple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorSimple : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody=GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = rBody.position;
+        rBody.position -= transform.right * speed * Time.fixedDeltaTime; //y軸無論是什麼，物體都能沿著傳送帶導軌方向移動
+        rBody.MovePosition(pos);
+    }
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
